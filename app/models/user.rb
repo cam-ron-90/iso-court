d7b0cbc917c4830a2678eb4fb6aba0dff076f1e0
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-04-10 22:16:25 +1000

    front-ent work on sign up sign in and reset password pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, presence: :true
+  validates :email, presence: :true
+  validates :password, presence: :true
+
   has_one_attached :avatar
 
 end
