25aabbaa47fe6af0e2c81f21da00bf92766f2d99
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-04-16 16:00:00 +1000

    fixed scrollbar issue, now default bottom

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -22,9 +22,9 @@ if (messagesContainer) {
 
   consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
     received(data) {
-      messagesContainer.insertAdjacentHTML('beforeend', data); // called when data is broadcast in the cable
+      messagesContainer.insertAdjacentHTML('afterbegin', data); // called when data is broadcast in the cable
       let blocs = messagesContainer.querySelectorAll(".bloc");
-      let last = blocs[blocs.length - 1];
+      let last = blocs[0];
       styling(last, userId);
     }
   });
