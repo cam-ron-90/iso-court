ae3dcf020e282967f40850a75667eaad5b78eee8
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-04-16 15:04:32 +1000

    chat works, dynamic styling works, needs to fix scroll

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,7 +1,21 @@
 // app/javascript/channels/chatroom_channel.js
 import consumer from "./consumer";
 
-const messagesContainer = document.getElementById('messages');
+const messagesContainer = document.getElementById("messages");
+const userId = messagesContainer.dataset["currentUser"]
+const input = document.querySelector('#message_content');
+
+const styling = (bloc, id) => {
+    if (bloc.dataset["userId"] === id) {
+      bloc.classList.add("message");
+      bloc.querySelector(".content").classList.add("message-content");
+      bloc.querySelector(".text").classList.add("right-text");
+    } else {
+      bloc.classList.add("message-left");
+      bloc.querySelector(".content").classList.add("message-content-left");
+      bloc.querySelector(".text").classList.add("left-text");
+    }
+  };
 
 if (messagesContainer) {
   const id = messagesContainer.dataset.chatroomId;
@@ -9,7 +23,9 @@ if (messagesContainer) {
   consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
     received(data) {
       messagesContainer.insertAdjacentHTML('beforeend', data); // called when data is broadcast in the cable
+      let blocs = messagesContainer.querySelectorAll(".bloc");
+      let last = blocs[blocs.length - 1];
+      styling(last, userId);
     }
   });
 }
-
