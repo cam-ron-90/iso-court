f5b6d76912b6da6f42776defbd0295839f674c4c
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-04-23 14:10:43 +1000

    added hearings history

--- app/controllers/hearings_controller.rb
+++ app/controllers/hearings_controller.rb
@@ -1,4 +1,10 @@
 class HearingsController < ApplicationController
+  def index
+    @hearings = Hearing.where(group: params[:group_id])
+    # NEEDS TO BE SORTED BY REVERSE CHRONOLOGICAL ORDER
+    # CURRENT HEARING SHOULDN'T BE DISPLAYED
+  end
+
   def show
     @hearing = Hearing.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :groups, except: [:edit, :update] do
     resources :charges, only: [:new, :create]
-    resources :hearings, only: [:show]
+    resources :hearings, only: [:index, :show]
   end
 
   resources :chatrooms, only: :show do
