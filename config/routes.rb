918d0aeb8277173cace564a0ebddf8dd300478b3
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-04-25 19:49:12 +1000

    group carousel on dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   get 'commitments/update'
 
   devise_for :users
-  root to: 'pages#home'
+  root to: 'groups#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :groups, except: [:edit, :update] do
     resources :charges, only: [:new, :create]
