56715af7395f53778ee67232d9a2c0926c0b0c71
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-05-20 18:16:38 +1000

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'groups#index'
+
+  resources :users, only: [:show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :groups, except: [:edit, :update] do
     resources :commitments, only: [:new, :create]
